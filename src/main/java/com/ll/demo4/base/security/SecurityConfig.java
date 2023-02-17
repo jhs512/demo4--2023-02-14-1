6ffd385514542ce2bead40b42752fd5ca9ce4318
jhs512 <jangka512@gmail.com>
2023-02-17 18:56:25 +0900

    work

--- src/main/java/com/ll/demo4/base/security/SecurityConfig.java
+++ src/main/java/com/ll/demo4/base/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.ll.demo4.base.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +17,23 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @RequiredArgsConstructor
 @Slf4j
 public class SecurityConfig {
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        authorizeHttpRequests -> authorizeHttpRequests
+                                .requestMatchers(
+                                        "/error",
+                                        "/recourse/**",
+                                        "/h2/**",
+                                        "/favicon.ico",
+                                        "/gen/**",
+                                        "/actuator/**"
+                                ).permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .formLogin(withDefaults());
+        return http.build();
+    }
 }
